1c2e4324e63e7d94bb55f02c6cad8d540e74e822
Johannes Frank <jmfrank63@gmail.com>
2014-12-04 20:04:19 +0000

    first lesson datatypes finished

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,77 @@
 // $("#main").append(["Johannes Maria Frank"]);
-var firstName = "Johannes"; 
-var age = 51;
-console.log(firstName);
+// var firstName = "Johannes"; 
+// var age = 51;
+// console.log(firstName);
+// var awesomeThoughts = "I am Johannes and I am AWESOME!";
+// // console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+// $("#main").append([funThoughts]);
+
+// var formattedName = HTMLheaderName.replace("%data%", "Johannes Maria Frank");
+// var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Ninja");
+
+// $("#header").prepend([formattedRole]);
+// $("#header").prepend([formattedName]);
+
+// var skills = ["awesomeness", "programming", "teaching", "JS"];
+
+// $("#main").append(skills);
+// $("#main").append(skills[0])
+
+// $("#main").append([skills.length]);
+
+// var bio = {
+//     "name" : "Johannes",
+//     "role" : "Frontend-End Ninja",
+//     "contact" : { "address": "Koenigsberger Str. 19b",
+//                   "postalCode" : "76139",
+//                   "city" : "Karlsruhe",
+//                   "country" : "Germany",
+//                   "email" : "jmfrank63",
+//                   "github" : "https://github.com/jmfrank63"
+//                 },
+//     "pictureURL" : "<img src='images/JMF.jpg' alt='Picture of Johannes'>",
+//     "age" : 51,
+//     "welcomeMessage" : "Welcome to my personal bio page.",
+//     "skills" : skills
+// };
+
+// with (bio) {
+//     $("#main").append(name, role, contact.city, contact.address, age, welcomeMessage, skills, [pictureURL]);
+// }
+
+// var work = {};
+// var education = {};
+
+// work.position = "Ninja";
+// work.year = 6;
+// work.city = "Nowhere";
+
+// education["school"] = "Oak Grove High School";
+// education["years"] = 4;
+// education["city"] = "San Jose";
+
+// $("#main").append(work["position"], education.school);
+
+// var education  = {
+//     "schools" : {
+//         "primary" : {
+//             "Grundschule Gaisburg" : "Stuttgart-Gaisburg",
+//             "Johann-Peter Hebel Grundschule" : "Karlsruhe",
+//             "Leopold Grundschule" : "Karlsruhe"
+//         },
+//         "secondary" : {
+//             "Bismark Gymnasium" : "Karlsruhe",
+//             "Gymnasium Laupheim" : "Laupheim",
+//             "Humboldt Gymnasium" : "Ulm"
+//         }
+//     },
+//     "majors" : {
+//         "Abitur" : "Humboldt Gymnasium Ulm",
+//         "Diplom" : "Fachhochschule der Deutschen Bundespost Berlin",
+//         "Nanodegree" : "Udacity",
+//         "Capstone" : "Coursera",
+//         "Linux" : "EdX"
+//     }
+// }
+
